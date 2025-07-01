6ec72f580ecaa87c146676f3525591873da3f7e3
Phile89 <Philellisonmusic@gmail.com>
2025-07-01 11:58:02 +0100

    SMS fix

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ console.log('PORT:', process.env.PORT || 3000);
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the root directory
 app.use(express.static(__dirname));
